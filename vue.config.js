6c5e9a5c219d7e399d2325b1a0227bc65e4dec77
dorian-grst <dorian.grasset.contact@gmail.com>
2022-12-31 01:57:56 +0100

    adding publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath : publicPath,
+  publicPath : '',
   pluginOptions: {
     i18n: {
       locale: 'fr',
